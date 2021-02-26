242f9bcfd5bd84c40cea3c0a339fcf4ac32ad7c3
Daneel <danel_1500@mail.ru>
2021-02-26 16:12:54 +0300

    new_commit

--- fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
+++ fclient/app/src/main/java/ru/iu3/fclient/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("native-lib");
         System.loadLibrary("mbedcrypto");
+        initRng();
     }
 
     @Override
@@ -19,8 +21,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        // TextView tv = findViewById(R.id.sample_text);
+        // tv.setText(stringFromJNI());
+
+        byte[] key = randomBytes(16);
+        byte[] data = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+        byte[] encrypted = encrypt(key, data);
+        byte[] decrypted = decrypt(key, encrypted);
+        String originalData  = new String(data, StandardCharsets.UTF_8);
+        String encryptedData = new String(encrypted, StandardCharsets.UTF_8);
+        String decryptedData = new String(decrypted, StandardCharsets.UTF_8);
+
+        System.out.println(originalData);
+        System.out.println(encryptedData);
+        System.out.println(decryptedData);
+
+        String output = new String(
+                "Original: "  + originalData  + "\n" +
+                        "Encrypted: " + encryptedData + "\n" +
+                        "Decrypted: " + decryptedData + "\0"
+        );
+
+        System.out.println("Original: "  + originalData);
+        System.out.println("Encrypted: " + encryptedData);
+        System.out.println("Decrypted: " + decryptedData);
     }
 
     /**
@@ -28,4 +52,9 @@ public class MainActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+    public static native int initRng();
+    public static native byte[] randomBytes(int n);
+
+    public static native byte[] encrypt(byte[] key, byte[] data);
+    public static native byte[] decrypt(byte[] key, byte[] data);
 }
